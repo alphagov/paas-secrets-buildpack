dc0c4acda0ea8ac443229af73d1fb3d3a639e185
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2020-10-16 16:36:16 +0100

    Supply installs Vault and generates the runtime scripts
    We have to generate scripts for two reasons:
    1. Fetching secrets can only be done at runtime, once the app instance identity
    certificate has been generated
    
    2. We need to know, at runtime, which directory in `/home/vcap/deps` holds the
    scripts
    
    This approach is perhaps a little confusing, but it neatly avoids ever knowing
    the secret values during staging.

---
+++ scripts/generate-agent-config.rb
@@ -0,0 +1,36 @@
+home = ENV["HOME"]
+secrets_url = ENV["SECRETS_API"]
+tmp_dir = ENV["TMPDIR"]
+
+puts %Q{
+pid_file = "#{home}/agent-pidfile"
+
+vault {
+    address = "#{secrets_url}"
+}
+
+listener "tcp" {
+    address = "localhost:9999"
+    tls_disable = true
+}
+
+cache {
+    use_auto_auth_token = true
+}
+
+auto_auth {
+    method "cf" {
+        config = {
+            role = "apps"
+        }
+    }
+
+    sink "file" {
+        wrap_ttl = "5m"
+
+        config = {
+            path = "#{tmp_dir}/vault-token"
+        }
+    }
+}
+}
